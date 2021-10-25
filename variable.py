3f8f10652a449a5445e5e504f4fb933164b1dc72
magusin <fanatsy4@gmail.com>
2021-10-26 01:02:59 +0200

    variable.py

---
+++ variable.py
@@ -0,0 +1,91 @@
+# Assigne le chiffre 9 à b 
+b = 9
+
+# Assigne la valeur 12 à c 
+c = 12
+
+# Assigne la valeur -2 à d
+d = -2
+
+# Affiche la variable b, c, d
+print(b, c, d)
+
+# Assigne la valeur 100 à A
+A = 100
+
+# Assigne la valeur 20 à B 
+B = 20
+
+# Assigne la valeur de A à C
+C = A
+
+# Assigne la valeur 120 à A
+A = 120
+
+# Assigne la valeur de A à D
+D = A
+
+# Affiche la variable A, B, C, D
+print('Le résultat de la variable A est :' , A)
+print('Le résultat de la variable B est :' , B)
+print('Le résultat de la variable C est :' , C)
+print('Le résultat de la variable D est :' , D)
+
+# Le résultat de la variable A est : 120                                                   
+# Le résultat de la variable B est : 20                                                    
+# Le résultat de la variable C est : 100                                                   
+# Le résultat de la variable D est : 120 
+
+# Assigne 26 à la variable A
+A = 26
+
+# Assigne  42 à la variable B
+B = 42
+
+# Assigne l'addition de A et B à la variable result
+result = A + B
+
+
+# Affiche result
+print(result)
+
+debut_phrase = "Voici"
+
+milieu_phrase = " ma"
+
+fin_phrase = " phrase"
+
+# Assigne à la variable phrase les trois autres variables
+phrase = debut_phrase + milieu_phrase + fin_phrase
+
+#Affiche la phrase
+print(phrase)
+
+first_name = "Nagini"
+print("Le prenom est", first_name)
+
+
+height = 286
+print("La longueur de la maison est", height)
+
+
+width = 4
+print("La largeur de la maison est", width)
+
+
+area = height * width
+
+print("La surface de la maison de", first_name, "est de", area, "m2")
+
+prenom = "Nagini"
+
+isSunshine = True
+# boolean avec une Maj
+myBitcoin = 0.230323
+
+print(prenom)
+print(isSunshine)
+print(myBitcoin)
+
+
+  
