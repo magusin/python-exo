2989c4cd0832b891352bc7e0dd3a6716283aa630
magusin <fanatsy4@gmail.com>
2021-10-26 00:37:33 +0200

    main.py end

--- main.py
+++ main.py
@@ -21,3 +21,30 @@ print((-60)/(-10))
 # Affiche le résultat du calcul 4 puissance 3
 print ( 4 ** 3)
 
+# Affiche le nombres d'élèves
+print ("Nombres d'élèves : ")
+print (293)
+
+# La méthode int() est utilisée pour arrondir un nombre flottant !
+# Affiche le nombre d'équipes 
+print("Le nombre d'équipes est :")
+print(int( 293 / 20))
+
+# Le modulo se notant %, donnera la valeur exacte restante d'une division!
+# Affiche le nombre de joueurs sans équipe
+print("Le nombre de joueur sans équipe est :")
+print( 293 % 20 )
+
+a = 3
+b = 4
+c = 6
+
+print(a,'-',b, "-",c)
+# donne 3 - 4 - 6
+
+# Depuis la version 3 de Python sous pouvez séparer les éléments d'un print() par le symbole de votre choix.
+# En l'indiquant par un sep="symbole"
+
+print(a,b,c , sep=" - ")
+
+
