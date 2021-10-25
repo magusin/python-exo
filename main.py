d1ba68c25e7fbce5b784b6eee6907eff42968fcc
magusin <fanatsy4@gmail.com>
2021-10-25 23:25:28 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+msg = "Hello World"
+print(msg)
+# Pour afficher un message et non une variable on utilise des guillemets
+# Print pour afficher
+
+# Affiche le résultat de 6 + 3 
+print(6 + 3 )
+
+# Affiche le résultat de 36 - 11 
+print( 36 - 11 )
+
+# Affiche le résultat de  8 - (-2) 
+print( 8 - (-2))
+
+# Affiche le résultat de -3 * 2 
+print((-3)*2 )
+
+# Affiche le résultat de  (-60) / (-10) 
+print((-60)/(-10))
+
+# Affiche le résultat du calcul 4 puissance 3
+print ( 4 ** 3)
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+Exercice et prise en main de python
+
+// Extension Python vscode
+windows: https://www.python.org/downloads/
+
+python nom-du-fichier 
+pour exécuter le fichier
