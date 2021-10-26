d96e18208d32ca2c2f4fca60176eab9b6997b4df
magusin <fanatsy4@gmail.com>
2021-10-26 02:38:23 +0200

    type-variable

---
+++ type-variable.py
@@ -0,0 +1,65 @@
+# La méthode type() permet de donner le type d'une variable!
+
+# Assigne la phrase "Salut voici un texte" a la variable message
+message = "Salut voici un texte"
+
+# Affiche message à l'aide d'un print()
+print(message)
+
+# Affiche le type de la variable message
+print(type(message))
+
+# Écris un message entre les ""
+message = "Prénom ici"
+
+# Affiche message à l'aide d'un print()
+print(message)
+# Assigne à t_msg le type de message
+t_msg = type(message)
+
+# Affiche t_msg
+print(t_msg)
+
+# variables
+age = 21
+adress_number = 75000
+
+#type des variables
+type_age = type(age)
+type_adress_number = type(adress_number)
+
+# Affiche les deux types à la suite
+print(type_age , type_adress_number)
+
+# Voici les Variables: x,y  et z
+x = 4
+y = 5.4
+z = x + y
+
+# Récupère les types des variables x, y et z
+type_x = type(x)
+type_y = type(y)
+type_z = type(z)
+
+# Affiche le type des variables x, y et z
+print("type de x : ", type_x)
+print("type de y : ", type_y)
+print("type de z : ", type_z) 
+#float nombre à virgule, s'écrit avec un . en py
+
+# Voici les Variables 
+# is_sunshine :pour savoir si le ciel est ensoleillé
+is_sunshine = False
+# be_happy :pour te dire d'être heureux
+be_happy = True
+
+# Récupére les Types de ces variables
+type_is_sunshine = type(is_sunshine)
+type_be_happy = type(be_happy)
+
+# Affiche les Types de ces variables
+print(type_is_sunshine , type_be_happy)
+# boolean True ou False
+
+
+
